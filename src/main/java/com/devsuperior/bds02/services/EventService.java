1b69b9cda3f130ea68ff7bf4a9158a7247d8897e
luciano-mota <amoosenhorjesus3@gmail.com>
2023-05-20 23:24:44 -0300

    implementado métodos nas classes

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.exceptions.IsNotFoundException;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public EventDTO updateEvent(EventDTO eventDTO, Long id) {
+        return new EventDTO(repository.save(buildEvent(eventDTO, id)));
+    }
+
+    private Event buildEvent(EventDTO eventDTO, Long id) {
+        Event event = repository.findById(id).orElseThrow(() -> new IsNotFoundException("ID not found"));
+
+        City city = cityRepository.findById(eventDTO.getCityId()).orElseThrow(() -> new IsNotFoundException("ID not found"));
+
+        event.setName(eventDTO.getName());
+        event.setDate(eventDTO.getDate());
+        event.setUrl(eventDTO.getUrl());
+        event.setCity(city);
+
+        return event;
+    }
+
+}
